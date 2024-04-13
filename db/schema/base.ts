2397d18f7c354eb20ebda23126e1e64f1957b78a
汪晓杰 <wangxiaojie@puluotech.com>
2024-04-13 14:50:12 +0800

    feat: 基础表添加isDelete

--- db/schema/base.ts
+++ db/schema/base.ts
@@ -1,8 +1,9 @@
 import { sql } from 'drizzle-orm';
-import { serial, datetime } from 'drizzle-orm/mysql-core';
+import { serial, datetime, boolean } from 'drizzle-orm/mysql-core';
 
 export const baseColumns = {
   id: serial('id').primaryKey(),
   createTime: datetime('createTime', { mode: 'string' }).default(sql`now()`),
   updateTime: datetime('updateTime', { mode: 'string' }).default(sql`now()`),
+  isDelete: boolean('is_delete').default(false),
 };
